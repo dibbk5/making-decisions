ba88194323c1f02e31df86367a3c62e196dce97c
Koy Dibb <dibbk5@gmail.com>
2022-11-15 19:29:10 -0700

    Questions 1-3

--- lab.js
+++ lab.js
@@ -58,7 +58,7 @@ let rain = true;
   If it is 80 degrees or above, and raining, one should wear a t-shirt and take an umbrella. If it is between 60 and 80 degrees, and raining, one should wear a rain-jacket. If it is 60 degrees or below, one should wear a jacket and carry an umbrella. Come up with your own clothing suggestions based on those temperature parameters when it is not raining.
 */
 
-//CODE HERE
+//CODE HERE 
 
 ////////// PROBLEM 5 //////////
 
